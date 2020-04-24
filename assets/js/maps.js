c088230a6a050a64aa11001386b381f3e5873690
vahe galoustian <galoustianv@gmail.com>
2020-04-24 03:41:35 +0000

    add Google Maps with Markers

---
+++ assets/js/maps.js
@@ -0,0 +1,26 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+         lat: 34.0522, lng: -118.2437
+      }
+    });
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [
+         {lat: 34.0522, lng: -118.2437},
+        { lat: 32.7157, lng: -117.1611},
+        { lat: 36.1699, lng: -115.1398 },
+        { lat: 37.7749, lng: -122.4194 }
+    ];
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+}
